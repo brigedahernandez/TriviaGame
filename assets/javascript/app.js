fd44b0c3aba0b98a56ab12226b22dfb86d64b2fc
Brigeda Hernandez <brigedahernandez@gmail.com>
2018-07-05 02:49:06 -0500

    I have a timer and a start button but they're not synced up yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,32 @@
-alert("Hello!");
+//TIMER 
+
+var number = 60;
+var intervalId;
+
+$("document").ready(function () {
+
+    
+    $("#start").on("click", run);
+
+
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+run();
+    function decrement() {
+        number--;
+
+        $("#timer").html("<h2>" + number + "</h2>");
+
+        if (number === 0) {
+            stop();
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+
+        
+    }
+})
