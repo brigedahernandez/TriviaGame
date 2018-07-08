a978fa58bfb1c38635bda26293165043251d20a3
Brigeda Hernandez <brigedahernandez@gmail.com>
2018-07-07 23:43:09 -0500

    still can't get the results to show up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var number = 60;
 var intervalId;
 var startbutton = document.getElementById("start");
 var submitbutton = document.getElementById("submit");
-var correctanswers = ["Teal","fightclub"]
+var correctanswers = ["Teal", "fightclub"]
 
 //when the page loads, questions and results wil be hidden.
 $("document").ready(function () {
@@ -11,20 +11,21 @@ $("document").ready(function () {
     $("#results").hide();
     $("#submitanswers").hide();
 
-//when the start button is clicked,the questions and submit button will appear
+    //when the start button is clicked,the questions and submit button will appear
     startbutton.onclick = function run() {
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
         $("#questions").show();
         $("#submitanswers").show();
+        $("#start").hide();
     }
-//If I take this out, the timer doesn't work... 
+    //If I take this out, the timer doesn't work... 
     submitanswers.onclick = function stop() {
         $("#results").show();
         $("#questions").hide();
     }
-    
-//function to subtract from and stop timer
+
+    //function to subtract from and stop timer
     function decrement() {
         number--;
 
@@ -36,28 +37,29 @@ $("document").ready(function () {
         }
     }
     //When submit button is pressed, it will loop through the array and compare user answers with correct answers. 
-function showResults (){
+    function showResults() {
+        preventDefault();
 
-    var userAnswer = "";
-    var numCorrect = 0;
+        var userAnswer = "";
+        var numCorrect = 0;
 
-for (var i=0; i<correctanswers.length; i++);
-    userAnswer = $('#radio').on("click");
-    if(userAnswer===correctanswers[i]){
+        for (var i = 0; i < correctanswers.length; i++);
+        userAnswer = $('#radio').on("click");
+        if (userAnswer === correctanswers[i]) {
 
-        numCorrect++;
-    
-    results.innerHTML = numCorrect + "out of" + correctanswers.length
-$("#results").show();
-}
+            numCorrect++;
 
-
-
+            $("#results").show();
+            $("#results").html(numCorrect, "Number Correct");
+        }
 
 
-}
-//and then it will show the results 
-submitanswers.onclick = function() {
-    showResults();
-}
+
+
+
+    }
+    //and then it will show the results IDEALLY 
+    submitanswers.onclick = function () {
+        showResults();
+    }
 })
